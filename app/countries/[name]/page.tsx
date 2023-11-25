c05683e502a3500e83ad56dceff04c227c132077
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2023-11-25 12:12:17 -0300

    :sparkles: feat: created country page

---
+++ app/countries/[name]/page.tsx
@@ -0,0 +1,121 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import type { Country } from "@/app/page";
+import CardCountries from "@/components/card-countries";
+
+async function getCountryByName(name: string): Promise<Country> {
+  const response: any = await fetch(`https://restcountries.com/v3.1/all`);
+
+  const data: Country[] = await response.json();
+
+  return data.find((country: Country) => country.name.common === name)!;
+}
+
+async function getCountryBordersByName(name: string) {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const countries: Country[] = await response.json();
+
+  const country = countries.find(
+    (country: Country) => country.name.common === name
+  )!;
+
+  return country.borders?.map((border) => {
+    const borderCountry = countries.find((country) => country.cca3 === border)!;
+    return {
+      name: borderCountry.name.common,
+      ptName: borderCountry.translations.por.common,
+      flag: borderCountry.flags.svg,
+      flagAlt: borderCountry.flags.alt,
+    };
+  });
+}
+
+export default async function Countrie({
+  params: { name },
+}: {
+  params: { name: string };
+}) {
+  const country: any = await getCountryByName(decodeURI(name));
+  const borderCountries = await getCountryBordersByName(decodeURI(name));
+  const formatter = Intl.NumberFormat("en", { notation: "compact" });
+
+  return (
+    <section className="container py-7">
+      <h1 className="text-center mb-2 text-5xl font-bold text-black">
+        {country.translations.por.common}
+      </h1>
+
+      <Link
+        href="/"
+        className="flex items-center text-base mb-2 font-normal text-black gap-1"
+      >
+        <Image src="/arrow-left.svg" width={24} height={24} alt="arrow left" />
+        Voltar
+      </Link>
+
+      <article className="w-full bg-white rounded-2xl flex items-center justify-between p-12 mb-12">
+        <div>
+          {country?.capital?.length && (
+            <h2 className="text-xl text-gray-800">
+              <b>🏙️ Capital: </b>
+              {country.capital}
+            </h2>
+          )}
+
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🗺️ Continente: </b>
+            {country.region} {country.subregion && `- ${country.subregion}`}
+          </h2>
+
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>👨‍👩‍👧‍👦 População: </b>
+            {formatter.format(country.population)}
+          </h2>
+
+          {country?.languages && (
+            <h2 className="text-xl text-gray-800 mt-3">
+              <b>🗣️ Línguas faladas: </b>
+
+              {Object.values(country.languages).map((language: any) => (
+                <span
+                  key={language}
+                  className="inline-block px-2 bg-indigo-700 mr-2 text-white text-sm rounded-full"
+                >
+                  {language}
+                </span>
+              ))}
+            </h2>
+          )}
+        </div>
+
+        <figure>
+          <Image
+            className="rounded-lg object-cover"
+            src={country.flags.svg}
+            alt={country.flags.alt}
+            width={430}
+            height={275}
+          />
+        </figure>
+      </article>
+
+      {borderCountries && (
+        <>
+          <h3 className="text-2xl font-bold mb-6">
+            Países que fazem fronteira
+          </h3>
+          <article className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2">
+            {borderCountries?.map((border: any) => (
+              <CardCountries
+                countrieName={border.name}
+                flag={border.flag}
+                altFlag={border.flagAlt}
+              />
+            ))}
+          </article>
+        </>
+      )}
+    </section>
+  );
+}
